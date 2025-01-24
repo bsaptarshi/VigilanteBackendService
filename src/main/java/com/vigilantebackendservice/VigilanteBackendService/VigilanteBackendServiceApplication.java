33126a6a25f729decfadf6c830425224f0349e16
Saptarshi Bhattacharjee <bsaptarshi52@LAPTOP-M2NUETBS.localdomain>
2025-01-24 01:22:40 -0800

    Initialize Spring

---
+++ src/main/java/com/vigilantebackendservice/VigilanteBackendService/VigilanteBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vigilantebackendservice.VigilanteBackendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigilanteBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigilanteBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vigilantebackendservice/VigilanteBackendService/VigilanteBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vigilantebackendservice.VigilanteBackendService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigilanteBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
